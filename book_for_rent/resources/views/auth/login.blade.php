849be9f50b304ad4d3c623148774138d0e55f1d5
Intummadee <kataroja1@gmail.com>
2024-04-16 23:09:29 +0700

    login
    -modify left box

--- book_for_rent/resources/views/auth/login.blade.php
+++ book_for_rent/resources/views/auth/login.blade.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -26,25 +25,63 @@
     <script src="https://cdn.ckeditor.com/ckeditor5/41.3.0/classic/ckeditor.js"></script>
 
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap');
+
+        /* Font for Books Left */
+        @import url('https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap');
+
         * {
             margin: 0;
             padding: 0;
             box-sizing: border-box;
-
+            /* font-family: "Inter", sans-serif; */
         }
 
+        $bookAngle: 60deg;
+        $speed: 5s;
+        $borderColor: #555;
+
+        $images:
+            url('https://picsum.photos/420/300?random=1'),
+            url('https://picsum.photos/420/300?random=2'),
+            url('https://picsum.photos/420/300?random=3'),
+            url('https://picsum.photos/420/300?random=4'),
+            url('https://picsum.photos/420/300?random=5'),
+            url('https://picsum.photos/420/300?random=1');
+
+            // Preload to images
+            .imgLoader {
+            position: fixed;
+            animation: preLoad 1s steps(1);
+            width: 1px;
+            height: 1px;
+
+            @keyframes preLoad {
+                0% { background-image: url('https://picsum.photos/420/300?random=1'); }
+                10% { background-image: url('https://picsum.photos/420/300?random=2'); }
+                20% { background-image: url('https://picsum.photos/420/300?random=3'); }
+                30% { background-image: url('https://picsum.photos/420/300?random=4'); }
+                40% { background-image: url('https://picsum.photos/420/300?random=5'); }
+                100% { display: none; }
+            }
+            }
+
+
+
         body {
-            background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk0ay4mR7OywI9urP8mD1nUBMIgTI90iryOf5SpvlvA4sgnhMMFVesqouc6l97r1C58uo&usqp=CAU');
+            /* background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk0ay4mR7OywI9urP8mD1nUBMIgTI90iryOf5SpvlvA4sgnhMMFVesqouc6l97r1C58uo&usqp=CAU');
             background-repeat: no-repeat;
             background-size: cover;
             background-position: center;
             display: flex;
             justify-content: center;
             align-items: center;
-            min-height: 100vh;
-
+            min-height: 100vh; */
+            font-family: 'Poppins', sans-serif;
+            background: #ececec;
         }
-        #box{
+
+        #box {
             background: transparent;
             border: 2px solid rgba(255, 255, 255, .2);
             backdrop-filter: blur(20px);
@@ -52,7 +89,8 @@
             border-radius: 10px;
             /* padding: 30px 40px; */
         }
-        #title{
+
+        #title {
             display: flex;
             justify-content: center;
             font-size: 2rem;
@@ -60,96 +98,149 @@
             font-weight: bold;
             font-optical-sizing: auto;
         }
-        #text-inter{
+
+        #text-inter {
             color: black;
             font-family: "Inter", sans-serif;
         }
+
+
+        /*------------ Login container ------------*/
+        .box-area {
+            width: 930px;
+        }
+
+        /*------------ Right box ------------*/
+        .right-box {
+            padding: 40px 30px 40px 40px;
+        }
+
+        /*------------ Custom Placeholder ------------*/
+        ::placeholder {
+            font-size: 16px;
+        }
+
+        .rounded-4 {
+            border-radius: 20px;
+        }
+
+        .rounded-5 {
+            border-radius: 30px;
+        }
+
+        /*------------ For small screens------------*/
+        /* ความกว้าง ไม่เกิน 768 พิกเซล คือความกว้างน้อยกว่า 768 px  */
+        @media only screen and (max-width: 768px) {
+            .box-area {
+                margin: 0 10px;
+            }
+
+            .left-box {
+                height: 100px;
+                overflow: hidden;
+            }
+
+            .right-box {
+                padding: 20px;
+            }
+        }
     </style>
-
-
 </head>
 
 <body>
+    <!----------------------- Main Container -------------------------->
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
+        {{-- d-flex ถ้าตั้งไว้ที่ container ที่เป็นตัวแม่ ตัวลูกหรือพวก div ข้างใน container ก็จะได้รับ d-flex ไปด้วย ประหนึ่งตั้งไว้ที่แม่ เพื่อทำให้กล่องมัน flex ปล. defualt ของ flex direction คือ row --}}
+        <!----------------------- Login Container -------------------------->
+        <div class="row border rounded-5 p-3 bg-white shadow box-area">
+            <!--------------------------- Left Box ----------------------------->
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8" '>
-                <div class="card" id='box'>
-                    <div class="card-header" id='title'>{{ __('Login') }}</div>
-                    <div class="card-body" id='text-inter'>
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
 
-                            <div class="row mb-3">
-                                <label for="email"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+            <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background: #103cbe;">
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email"
-                                        class="form-control @error('email') is-invalid @enderror" name="email"
-                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                {{-- <div class="featured-image mb-3">
+                    <img src="images/1.png" class="img-fluid" style="width: 250px;">
+                </div> --}}
+
+                <div class="book">
+                    <div class="gap"></div>
+                    <div class="pages">
+                      <div class="page"></div>
+                      <div class="page"></div>
+                      <div class="page"></div>
+                      <div class="page"></div>
+                      <div class="page"></div>
+                      <div class="page"></div>
+                    </div>
+                    <div class="flips">
+                      <div class="flip flip1">
+                        <div class="flip flip2">
+                          <div class="flip flip3">
+                            <div class="flip flip4">
+                              <div class="flip flip5">
+                                <div class="flip flip6">
+                                  <div class="flip flip7"></div>
                                 </div>
+                              </div>
                             </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
 
-                            <div class="row mb-3">
-                                <label for="password"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
-                                <div class="col-md-6">
-                                    <input id="password" type="password"
-                                        class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="current-password">
 
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
 
-                            <div class="row mb-3">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                            {{ old('remember') ? 'checked' : '' }}>
 
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
+                <p class="text-white fs-2" style="font-family: 'Courier New', Courier, monospace; font-weight: 600;">Be Verified</p>
+                {{-- text-wrap มันเหมือนการห่อ ถ้าเกินว่า ตัวอักษรมันเกินขนาดกล่องที่มันอยู่ ถ้าไม่ตั้ง text-wrap ไว้ มันจะเกินขนาดออกไป แต่ถ้า wrap มันจะปัดตกมาเปนบรรทัดที่สอง เพื่อให้ตัวอักษรมันไม่หลุดออกนอกกล่องที่ text มันอยู่ --}}
+                <small class="text-white text-wrap text-center"
+                    style="width: 17rem;font-family: 'Courier New', Courier, monospace;">Join experienced Designers on
+                    this platform.</small>
+            </div>
+            <!-------------------- ------ Right Box ---------------------------->
 
-                            <div class="row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button style="font-family: "Inter", sans-serif;" type="submit" class="btn btn-primary">
-                                        {{ __('Login') }}
-                                    </button>
-
-                                    @if (Route::has('password.request'))
-                                        <a id='text-inter' class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                    @endif
-                                </div>
-                            </div>
-                        </form>
+            <div class="col-md-6 right-box">
+                <div class="row align-items-center">
+                    <div class="header-text mb-4">
+                        <h2>Hello,Again</h2>
+                        <p>We are happy to have you back.</p>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control form-control-lg bg-light fs-6"
+                            placeholder="Email address">
+                    </div>
+                    <div class="input-group mb-1">
+                        <input type="password" class="form-control form-control-lg bg-light fs-6"
+                            placeholder="Password">
+                    </div>
+                    <div class="input-group mb-5 d-flex justify-content-between">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="formCheck">
+                            <label for="formCheck" class="form-check-label text-secondary"><small>Remember
+                                    Me</small></label>
+                        </div>
+                        <div class="forgot">
+                            <small><a href="#">Forgot Password?</a></small>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <button class="btn btn-lg btn-primary w-100 fs-6">Login</button>
+                    </div>
+                    <div class="input-group mb-3">
+                        <button class="btn btn-lg btn-light w-100 fs-6"><img src="images/google.png" style="width:20px"
+                                class="me-2"><small>Sign In with Google</small></button>
+                    </div>
+                    <div class="row">
+                        <small>Don't have account? <a href="#">Sign Up</a></small>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-
-
-
 </body>
 
 </html>
